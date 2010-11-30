297fc59d930052d0ab825f877ed6b721cb85d744
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-12-01 00:06:45 +0100

    Refactor to class

---
+++ password_crypt.rb
@@ -0,0 +1,19 @@
+class PasswordCrypt
+  
+  def initialize(filename, case_sensitive)
+    @key = YAML::load(File.open(filename))
+    @case_sensitive = case_sensitive
+  end
+
+  def lookup(input)
+    input.split('').map do |char|
+      char = char.to_lower unless @case_sensitive
+      if @key.has_key? char
+        @key[char]
+      else
+        char
+      end
+    end.join
+  end
+
+end
