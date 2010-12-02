22a779cbf9e1d38ee77cb2c67ace6f763817e558
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-12-02 19:04:00 +0100

    Add breaking test

--- spec/password_crypt_spec.rb
+++ spec/password_crypt_spec.rb
@@ -1,8 +1,8 @@
-require './password_crypt.rb'
+require './src/password_crypt.rb'
 
 describe PasswordCrypt, '#initialize' do 
   it 'should use default key file if none given' do 
     pc = PasswordCrypt.new
-    pc.keyfile.should == PasswordCrypt.DEFAULT_KEY_FILE 
+    pc.keyfile.should == PasswordCrypt::DEFAULT_KEY_FILE 
   end
 end
