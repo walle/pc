70eb5175f05c4dedc34e9f888e3b8964e1acf2bb
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-12-02 19:13:28 +0100

    Set correct description

--- spec/password_crypt_spec.rb
+++ spec/password_crypt_spec.rb
@@ -1,6 +1,6 @@
 require './src/password_crypt.rb'
 
-describe PasswordCrypt, '#initialize' do 
+describe PasswordCrypt do 
   it 'should use default key file if none given' do 
     pc = PasswordCrypt.new
     pc.keyfile.should == PasswordCrypt::DEFAULT_KEY_FILE 
