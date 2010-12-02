82f12ff6bc1662da21c2e56c3c6ebabcf998ebf0
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-12-02 19:46:16 +0100

    Add more passing tests

--- spec/password_crypt_spec.rb
+++ spec/password_crypt_spec.rb
@@ -1,23 +1,28 @@
 require './src/password_crypt.rb'
 
 describe PasswordCrypt do 
+  before(:each) do 
+    @pc = PasswordCrypt.new 
+  end
+
   it 'should use default key file if none given' do 
-    pc = PasswordCrypt.new
-    pc.keyfile.should == PasswordCrypt::DEFAULT_KEY_FILE 
+    @pc.keyfile.should == PasswordCrypt::DEFAULT_KEY_FILE 
   end
   
   it 'should not be case sensitive unless said so' do 
-    pc = PasswordCrypt.new
-    pc.case_sensitive.should be_false
+    @pc.case_sensitive.should be_false
   end
 
   it 'should be case sensitive when told' do 
-    pc = PasswordCrypt.new({:case_sensitive => true}) 
-    pc.case_sensitive.should be_true
+    @pc = PasswordCrypt.new({:case_sensitive => true}) 
+    @pc.case_sensitive.should be_true
   end
 
   it 'should respond to lookup' do 
-    pc = PasswordCrypt.new
-    pc.should respond_to :lookup
+    @pc.should respond_to :lookup
+  end
+
+  it 'should not respond to key' do 
+    @pc.should_not respond_to :key  
   end
 end
